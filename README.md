c217c53476fac7e4b74e79cefd018b7cfa053332
Carl Stephens <carlst99@outlook.co.nz>
2022-01-22 10:30:45 +1300

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # ZlibNGSharpMinimal
 
-A C# interop wrapper for [zlib-ng's](https://github.com/zlib-ng/zlib-ng) basic inflate and deflate operations.
+A .NET 6 C# interop wrapper for [zlib-ng's](https://github.com/zlib-ng/zlib-ng) basic inflate and deflate operations.
 Written for and bundled with zlib-ng v2.0.6.
+
+```csharp
+byte[] data = new byte[512];
+byte[] deflated = new byte[Data.Length];
+byte[] inflated = new byte[Data.Length];
+
+for (int i = 0; i < data.Length; i++)
+    data[i] = (byte)(i % 8);
+
+using ZngDeflater deflater = new();
+deflater.Deflate(Data, deflated);
+
+using ZngInflater inflater = new();
+inflater.Inflate(deflated, inflated);
+```
--- ZlibNGSharpMinimal.Tests/DllImportHelper.cs
+++ ZlibNGSharpMinimal.Tests/DllImportHelper.cs
@@ -9,14 +9,15 @@ public static class DllImportHelper
 {
     public static IntPtr ZngDllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
     {
+        const string nativeFolder = "native";
         string libPath = "runtimes";
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            libPath = Path.Combine(libPath, "win-x64", "native", libraryName + ".dll");
+            libPath = Path.Combine(libPath, "win-x64", nativeFolder, libraryName + ".dll");
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            libPath = Path.Combine(libPath, "linux-x64", "native", libraryName + ".so");
+            libPath = Path.Combine(libPath, "linux-x64", nativeFolder, libraryName + ".so");
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            libPath = Path.Combine(libPath, "osx-x64", "native", libraryName + ".dylib");
+            libPath = Path.Combine(libPath, "osx-x64", nativeFolder, libraryName + ".dylib");
 
         if (NativeLibrary.TryLoad(libPath, out IntPtr libPtr))
             return libPtr;
